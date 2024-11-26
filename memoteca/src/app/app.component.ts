20e778c6951150e3cb93cc3010d769360c0ceefe
victorcosmedev <victorcosme.profissional@gmail.com>
2024-11-26 20:37:20 -0300

    Property binding funcionando com sucesso.

--- memoteca/src/app/app.component.ts
+++ memoteca/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
 import { RodapeComponent } from './components/rodape/rodape.component';
+import { CriarPensamentoComponent } from "./components/pensamentos/criar-pensamento/criar-pensamento.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CabecalhoComponent, RodapeComponent],
+  imports: [RouterOutlet, CabecalhoComponent, RodapeComponent, CriarPensamentoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
